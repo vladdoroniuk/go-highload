01f4e241b044a9c3288b6344ebbe1946c1fb8d23
vladdoroniuk <vladyslav.doroniuk@gmail.com>
2025-03-15 17:01:02 +0200

    refactor: update customer-service docker compose

--- services/customers/deployments/postgres-init.sh
+++ services/customers/deployments/postgres-init.sh
@@ -2,13 +2,15 @@
 
 echo "Waiting for PostgreSQL to be ready..."
 
-until psql -h go-highload-postgres -U "$POSTGRES_USER" -d postgres -c "SELECT 1" &>/dev/null; do
+until pg_isready -h go-highload-postgres -U "$POSTGRES_USER"; do
     sleep 5
     echo "Still waiting for PostgreSQL..."
 done
 
 echo "PostgreSQL is ready! Creating database if not exists..."
-psql -h go-highload-postgres -U "$POSTGRES_USER" -d postgres -c "CREATE DATABASE $POSTGRES_DB;" || echo "Database $POSTGRES_DB already exists."
-echo "Granting privileges..."
-psql -h go-highload-postgres -U "$POSTGRES_USER" -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;"
+
+export PGPASSWORD="$POSTGRES_PASSWORD"
+psql -h go-highload-postgres -U "$POSTGRES_USER" -d postgres -tc "SELECT 1 FROM pg_database WHERE datname = '$POSTGRES_DB'" | grep -q 1 || \
+psql -h go-highload-postgres -U "$POSTGRES_USER" -d postgres -c "CREATE DATABASE $POSTGRES_DB;"
+
 echo "Done!"
--- services/inventory/deployments/cassandra-init.sh
+++ services/inventory/deployments/cassandra-init.sh
@@ -7,6 +7,8 @@ until cqlsh go-highload-cassandra -e "DESCRIBE KEYSPACES" &>/dev/null; do
     echo "Still waiting for Cassandra..."
 done
 
-echo "Cassandra is ready! Creating keyspace..."
+echo "Cassandra is ready! Creating keyspace if not exists..."
+
 cqlsh go-highload-cassandra -u "$CASSANDRA_USER" -p "$CASSANDRA_PASSWORD" -e "CREATE KEYSPACE IF NOT EXISTS $CASSANDRA_KEYSPACE WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};"
+
 echo "Done!"
